5e66d2f0ab6affecad84c4720ba5e1d6bddb9f67
Arman Ali Khan <74469015+arman-ali-khan@users.noreply.github.com>
2025-06-02 11:49:56 +0600

    Update product-client.tsx

--- app/product/[id]/product-client.tsx
+++ app/product/[id]/product-client.tsx
@@ -50,7 +50,7 @@ export default function ProductClient({ product }: { product: any }) {
           
           {product.images.length > 1 && (
             <div className="flex gap-2 overflow-auto pb-2">
-              {product.images.map((image: { url: string }, index) => (
+              {product.images.map((image: { url: string }, index:number) => (
                 <button
                   key={index}
                   className={`relative aspect-square h-20 overflow-hidden rounded-md border-2 ${
